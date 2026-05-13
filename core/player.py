63b63d023701b656442fab222a5fda56c1482f1f
AntonioMarcel <marcel.sotero@gmail.com>
2026-05-12 23:38:41 -0300

    feat: implement player hand management mechanics

---
+++ core/player.py
@@ -0,0 +1,21 @@
+from .card import Card
+
+class Player:
+    def __init__(self, name: str):
+        self.name = name
+        self.hand = []
+
+    def receive_card(self, card: Card):
+        self.hand.append(card)
+
+    def receive_cards(self, cards: list[Card]):
+        self.hand.extend(cards)
+
+    def has_cards(self):
+        return len(self.hand) > 0
+
+    def play_card(self):
+        return self.hand.pop(0)
+        
+    def __str__(self):
+        return f"{self.name} has {len(self.hand)} cards"
